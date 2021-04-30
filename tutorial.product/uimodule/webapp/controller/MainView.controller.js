37fa21efa130988c22d4527fe3900947a92f54c6
MANIRAJAN VARATHARAJAN <manirajan729155@gmail.com>
2021-04-30 17:19:04 +0000

    revert product

--- tutorial.product/uimodule/webapp/controller/MainView.controller.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define(["tutorial/product/controller/BaseController"], function (Controller) {
-    "use strict";
-
-    return Controller.extend("tutorial.product.controller.MainView", {});
-});
